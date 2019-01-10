866a334e737d4e527c1c4dc16a6fca17d51e359f
Aamir Pinger <43044635+aamirpinger@users.noreply.github.com>
2019-01-10 20:32:01 +0500

    App.js without calling MoviesAPI.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import ListMovies from './ListMovies';
 import FilterMovie from './FilterMovie'
-import * as MovieAPI from './utils/MoviesAPI'
 
 class App extends Component {
 
 
   state = {
-    movies: [],
+    movies: [
+      {
+        "id": "matrix",
+        "name": "Matrix",
+        "category": "fiction",
+        "posterURL": "https://c1.staticflickr.com/9/8819/17072638696_f871731849_n.jpg"
+      },
+      {
+        "id": "avengers",
+        "name": "Avengers Captain America Hawkeye",
+        "category": "Superhero",
+        "posterURL": "https:////c1.staticflickr.com/8/7215/6976089458_e23538854b_m.jpg"
+      },
+      {
+        "id": "amazingspiderman",
+        "name": "The Amazing Spider-man",
+        "category": "Superhero",
+        "posterURL": "https:////c1.staticflickr.com/8/7249/7088374885_d0061c5910_m.jpg"
+      }
+    ],
     criteria: ''  
-  }
-
-  componentDidMount(){
-    MovieAPI.getAll()
-    .then((moviesFromServer) => {
-      this.setState(() => ({ movies: moviesFromServer }))
-    })
   }
 
   removeMovie = (movie) => {
     this.setState((prevState) => ({
-      movies: prevState.movies.filter((m) => m.id !== movie )
+      movies: prevState.movies.filter((m) => m.name !== movie )
     }))
-
-    MovieAPI.remove(movie)
   }
 
   
